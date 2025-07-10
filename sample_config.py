c0781ab424f69b9a91782b1b0b8525d9dfabfba8
flamexmaster232 <movieexplainer9582@gmail.com>
2025-07-10 12:39:56 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,9 +1,9 @@
 import os
 
-api_id = 29683927
-api_hash = os.environ.get("API_HASH", "b2eb32ac1030edd8bc36c7b554ef6fc3")
-bot_token = os.environ.get("")
-auth_users = os.environ.get("AUTH_USERS", "5583858510")
-sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = os.environ.get("OWNER_USERS", "5583858510")
-owner_users = [int(num) for num in osowner_users.split(",")]
+api_id = 26548330
+api_hash = os.environ.get("API_HASH", "244c3afd019c7b0cd1a8184cd2be2495")
+bot_token = os.environ.get("7112965184:AAFOcOPfUswSABurDw99UryOU471fDoS0_4")
+auth_users = os.environ.get("AUTH_USERS", "6890753169")
+sudo_users = [int(num) for num in auth_users.split("6890753169")]
+osowner_users = os.environ.get("OWNER_USERS", "6890753169")
+owner_users = [int(num) for num in osowner_users.split("6890753169")]
